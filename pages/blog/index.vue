059712a52736baacde9b18d794cb153694036b79
unknown <hoanglt.gis@gmail.com>
2020-04-29 17:26:29 +0700

    init page blog

---
+++ pages/blog/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="blogs">
+    <h2>Blogs</h2>
+    <ul>
+      <li v-for="blog in blogs" :key="blog.id">
+        <b-button>{{ blog.name }}</b-button>
+        <hr />
+      </li>
+    </ul>
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: [
+        {
+          id: 1,
+          name: 'Intro VueJS'
+        },
+        {
+          id: 2,
+          name: 'Intro NuxtJS'
+        },
+        {
+          id: 3,
+          name: 'Advan NuxtJS'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -30,6 +30,18 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  head() {
+    return {
+      title: 'Home page 🚀',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Home page description'
+        }
+      ]
+    }
   }
 }
 </script>
